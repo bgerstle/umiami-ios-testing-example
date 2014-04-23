b0b91081f629765ee6c4e243c801a717211f3d0c
Brian Gerstle <brian.gerstle@gmail.com>
2014-04-22 22:27:33 -0400

    failing track list modification tests

--- SoundCloudSearchTests/SCHomeViewControllerTests.m
+++ SoundCloudSearchTests/SCHomeViewControllerTests.m
@@ -9,6 +9,22 @@
 #import <XCTest/XCTest.h>
 #import "SCHomeViewController.h"
 
+
+static id scs_createDummyTrackWithTitle(NSString *title)
+{
+    return @{@"title": title};
+}
+
+static NSArray* scs_createDummyTracks(NSArray *dummyTrackTitles)
+{
+    NSMutableArray * dummyTracks = [[NSMutableArray alloc] initWithCapacity:[dummyTrackTitles count]];
+    [dummyTrackTitles enumerateObjectsUsingBlock:^(NSString *dummyTitle, NSUInteger idx, BOOL *stop) {
+        id dummyTrack = scs_createDummyTrackWithTitle(dummyTitle);
+        [dummyTracks addObject:dummyTrack];
+    }];
+    return dummyTracks;
+}
+
 @interface SCHomeViewControllerTests : XCTestCase
 @property (nonatomic, strong) SCHomeViewController *homeViewController;
 @end
@@ -35,6 +51,36 @@
                   @"Can't find track list view!");
     
 	XCTAssertEqual([_homeViewController.trackListView numberOfRowsInSection:0], 0, @"should have 0 tracks in list");
+}
+
+- (void)testShouldShowCurrentTracksInList
+{
+    _homeViewController.tracks = [NSMutableArray new];
+    NSMutableArray *tracksProxy = [_homeViewController mutableArrayValueForKey:@"tracks"];
+    
+    NSArray *dummyTracks = scs_createDummyTracks(@[@"A", @"B"]);
+    
+    // addition
+    [tracksProxy addObjectsFromArray:dummyTracks];
+    XCTAssertEqual([_homeViewController.trackListView numberOfRowsInSection:0], 2,
+                   @"didn't update table after adding tracks");
+    
+    // substitution
+    NSDictionary *trackC = scs_createDummyTrackWithTitle(@"C");
+    [tracksProxy replaceObjectAtIndex:0 withObject:trackC];
+    XCTAssertEqual([_homeViewController.trackListView numberOfRowsInSection:0], 1,
+                   @"replacing should preserve number of rows.");
+    UITableViewCell *trackCCell = [_homeViewController.trackListView cellForRowAtIndexPath:
+                                   [NSIndexPath indexPathForRow:0 inSection:0]];
+    XCTAssertEqualObjects(trackCCell.textLabel.text, @"C", @"Should be showing track C since we replaced A");
+    
+    // deletion
+    [tracksProxy removeObjectAtIndex:1];
+    XCTAssertEqual([_homeViewController.trackListView numberOfRowsInSection:0], 1,
+                   @"should only have 1 row after removing a track");
+    UITableViewCell *trackBCell = [_homeViewController.trackListView cellForRowAtIndexPath:
+                                   [NSIndexPath indexPathForRow:0 inSection:0]];
+    XCTAssertEqualObjects(trackBCell.textLabel.text, @"B", @"Should be showing track C since we removed B");
 }
 
 @end
